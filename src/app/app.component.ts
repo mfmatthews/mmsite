52f8120c05cc1649e3ac2508a138f0544ca46f5a
mfmatthews <mike@webtiers.com>
2026-03-22 12:57:09 -0600

    improved for mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mmsite';
+  menuOpen = false;
+  toggleMenu() { this.menuOpen = !this.menuOpen; }
+  closeMenu() { this.menuOpen = false; }
 }
